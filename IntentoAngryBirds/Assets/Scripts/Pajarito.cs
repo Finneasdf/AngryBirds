e3b999674ebb0348a979ea421173153538a8e654
Finneasdf <199539@iberopuebla.mx>
2026-04-08 18:04:08 -0600

    Otra vez lo reinicie

--- IntentoAngryBirds/Assets/Scripts/Pajarito.cs
+++ IntentoAngryBirds/Assets/Scripts/Pajarito.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
+
 public class Pajarito : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Camera mainCamera;
-    private bool estoyArrastrando = false;
-    private Vector3 offset;
 
     void Start()
     {
@@ -13,36 +12,11 @@ public class Pajarito : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void OnMouseDrag()
     {
-        // Detectar click con Raycast 2D
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-
-            Debug.Log("Click en: " + mousePos + " | Hit: " + hit.collider);
-
-            if (hit.collider != null && hit.collider.gameObject == gameObject)
-            {
-                estoyArrastrando = true;
-                rb.gravityScale = 0;
-                rb.linearVelocity = Vector2.zero;
-                offset = transform.position - (Vector3)mousePos;
-                Debug.Log("Pajaro clickeado!");
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            estoyArrastrando = false;
-            rb.gravityScale = 1;
-        }
-
-        if (estoyArrastrando)
-        {
-            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = (Vector3)mousePos + offset;
-        }
+        
+        Vector3 dragPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        dragPosition.z = transform.position.z;
+        transform.position = dragPosition;
     }
 }
