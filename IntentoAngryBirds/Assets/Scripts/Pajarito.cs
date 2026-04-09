a4013b1e6344a95df57d0eec588d8df4ff8631c0
Finneasdf <199539@iberopuebla.mx>
2026-04-08 19:04:33 -0600

    Puse cerdito
    Le coloque una animación al cerdito y ahora voy a empezar a ver lo de las collisiones y como va a desaparecer

--- IntentoAngryBirds/Assets/Scripts/Pajarito.cs
+++ IntentoAngryBirds/Assets/Scripts/Pajarito.cs
@@ -5,11 +5,17 @@ public class Pajarito : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Camera mainCamera;
+    private Vector3 posicionInicial, limitePosicion;
+
+    public float force;
+    public float maxDis;
 
     void Start()
     {
         mainCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        posicionInicial = transform.position;
     }
 
     private void OnMouseDrag()
@@ -17,6 +23,34 @@ public class Pajarito : MonoBehaviour
         
         Vector3 dragPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         dragPosition.z = transform.position.z;
-        transform.position = dragPosition;
+        limitePosicion = dragPosition;
+
+        float dragDis = Vector3.Distance(posicionInicial , dragPosition);
+
+        if(dragDis > maxDis)
+        {
+            limitePosicion = posicionInicial + (dragPosition - posicionInicial).normalized * maxDis;
+        }
+
+        transform.position = limitePosicion;
+
+    }
+
+    private void OnMouseUp()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Vector3 throwVector = posicionInicial - limitePosicion;
+        rb.AddForce(throwVector * force);
+
+        float regreso = 5f;
+        Invoke("Reinicio", regreso);
+        
+    }
+
+    private void Reinicio()
+    {
+        transform.position = posicionInicial;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.linearVelocity = Vector3.zero;
     }
 }
